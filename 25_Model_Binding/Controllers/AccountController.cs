64c3a201f9c70bb6092fe03d8a49a702a808506f
4kumarabhishek <4kumar.abhishek@gmail.com>
2024-09-06 18:51:00 +0530

    Add project files.

---
+++ 25_Model_Binding/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _25_Model_Binding.Models;
+
+namespace _25_Model_Binding.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        //Primitive model binding
+
+        //[HttpPost]
+        //public ContentResult Login(string username, string password)
+        //{
+        //    if(username == "scott" && password == "tiger")
+        //    {
+        //        return Content("Welcome scott");
+        //    }else
+        //    {
+        //        return Content("Sorry " + username);
+        //    }
+        //}
+
+
+        //Complex model binding
+
+        [HttpPost]
+        public ContentResult Login(User model)
+        {
+            if (model.Username == "scott" && model.Password == "tiger")
+            {
+                return Content("Welcome scott");
+            }
+            else
+            {
+                return Content("Sorry " + model.Username);
+            }
+        }
+    }
+}
